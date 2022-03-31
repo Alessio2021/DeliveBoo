65a483ee9b3c5dbbaea80fc7212ae953496cb09c
Angeloamenta <angeloamenta7@gmail.com>
2022-03-31 18:14:26 +0200

    add img array categories

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -13,22 +13,60 @@ class CategorySeeder extends Seeder
     public function run()
     {
         $categories = [
-            'italiano',
-            'internazionale',
-            'cinese',
-            'giapponese',
-            'messicano',
-            'indiano',
-            'vegetariano',
-            'vegano',
-            'fast food',
-            'americano',
-            'colazione',
-            'greco',
+            [
+                'nome' => "italiano",
+                'img' => 'italian-icon.svg',
+            ],
+            [
+                'nome' => "internazionale",
+                'img' => 'international-icon.svg',
+            ],
+            [
+                'nome' => "cinese",
+                'img' => 'chinese-icon.svg',
+            ],
+            [
+                'nome' => "giapponese",
+                'img' => 'japan-icon.svg',
+            ],
+            [
+                'nome' => "messicano",
+                'img' => 'mexican-icon.svg',
+            ],
+            [
+                'nome' => "indiano",
+                'img' => 'indian-icon.svg',
+            ],
+            [
+                'nome' => "vegetariano",
+                'img' => 'vegetarian-icon.svg',
+            ],
+            [
+                'nome' => "vegano",
+                'img' => 'vegan-icon.svg',
+            ],
+            [
+                'nome' => "fast food",
+                'img' => 'fastfood-icon.svg',
+            ],
+            [
+                'nome' => "americano",
+                'img' => 'american-icon.svg',
+            ],
+            [
+                'nome' => "colazione",
+                'img' => 'breakfast-icon.svg',
+            ],
+            [
+                'nome' => "greco",
+                'img' => 'greek-icon.svg',
+            ],
+
         ];
         foreach ($categories as $category) {
             $newCategory = new Category();
-            $newCategory->name = $category;
+            $newCategory->name = $category['nome'];
+            $newCategory->image = $category['img'];
             $newCategory->save();
         }
     }
