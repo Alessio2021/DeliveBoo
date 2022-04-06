4236de0e61ad8572b2be133c1111a0334d9540ba
Valkyrux <philadephos.granata@gmail.com>
2022-04-06 17:08:11 +0200

    payement

---
+++ app/Http/Controllers/Checkout/PaymentController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Checkout;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Braintree\Gateway;
+
+use App\User;
+use App\Dish;
+use App\Order;
+
+class PaymentController extends Controller
+{
+    public function paymentFunction()
+    {
+        // if (!empty($_POST["data"]) && !empty($_POST["payment_method_nonce"]) && $_POST["address"]) {
+        if (!empty($_POST["data"]) && !empty($_POST["payment_method_nonce"])) {
+            $data = json_decode($_POST["data"], true);
+            $nonceFromTheClient = $_POST["payment_method_nonce"];
+            // $address = $_POST["address"];
+            $address = "CIAO";
+            $amount = 0;
+
+            $checkDishesOnDataBase = true;
+
+            $order = new Order();
+            $order->guest_address = $address;
+            // cambiare
+            $order->payment_method = "DioBastardo";
+
+            foreach ($data['dishes'] as $dish) {
+                $dishInOrder = Dish::where('user_id', User::where('slug', $data['restaurant']['slug'])->first()->id)->where('slug', $dish['slug'])->first();
+                if (!empty($dishInOrder)) {
+                    $amount += $dishInOrder->price * $dish['amount'];
+                } else {
+                    $checkDishesOnDataBase = false;
+                }
+            }
+
+            if ($checkDishesOnDataBase) {
+                $gateway = new Gateway([
+                    'environment' => 'sandbox',
+                    'merchantId' => '6kkmvpfqh8n88bbj',
+                    'publicKey' => 'bnct3wd7sk8scnsv',
+                    'privateKey' => '35c10fb92992c1374aa6e6aa95e86c7f'
+                ]);
+
+                $result = $gateway->transaction()->sale([
+                    'amount' => $amount,
+                    'paymentMethodNonce' => $nonceFromTheClient,
+                    'options' => [
+                        'submitForSettlement' => True
+                    ]
+                ]);
+
+                if ($result->success) {
+
+                    $order->total_amount = $amount;
+                    $order->save();
+
+                    foreach ($data['dishes'] as $dish) {
+                        $dishInOrder = Dish::where('user_id', User::where('slug', $data['restaurant']['slug'])->first()->id)->where('slug', $dish['slug'])->first();
+                        $order->dishes()->attach($dishInOrder, ['amount' => $dish['amount'], 'history_price' => $dishInOrder->price]);
+                    }
+
+                    return redirect()->route('guests')->with('status', "Pagamento Effettuato");
+                } else {
+                    return redirect()->route('guests')->with('status', "Problemi con il Pagamento");
+                }
+            } else {
+                return redirect()->route('guests')->with('status', "Ordine Non Valido");
+            }
+        } else {
+            return redirect()->route('guests');
+        }
+    }
+}
--- resources/js/components/ShopCart.vue
+++ resources/js/components/ShopCart.vue
@@ -65,6 +65,7 @@ export default {
     watch: {
         choosenDish() {
             this.addOnCart();
+            this.synchCartOnRequest();
         },
     },
     created() {
@@ -76,15 +77,16 @@ export default {
             localStorage.setItem('key', JSON.stringify(this.data));
         }
 
-        // JSON ALL'INIZIO
-
-        document.getElementById('JSONOrder').value = JSON.stringify(this.data);
+        this.synchCartOnRequest();
 
         this.$emit('restaurantOnCart', this.data.restaurant.slug);
     },
     methods: {
         ciao() {
             document.getElementById('payNow').click();
+        },
+        synchCartOnRequest() {
+            document.getElementById('JSONOrder').value = JSON.stringify(this.data);
         },
         addOnCart() {
             if ( this.choosenDish.restaurant.slug == this.data.restaurant.slug || this.data.restaurant.slug == '') {
@@ -151,7 +153,7 @@ export default {
             const dish = this.data.dishes.filter(dish => dish.slug == emitted.dishSlug);
             dish[0].amount = emitted.value;
             localStorage.setItem('key', JSON.stringify(this.data));
-            console.log(dish);
+            this.synchCartOnRequest();
         }
     }
 }
