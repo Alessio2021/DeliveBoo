1e832acd8002abff3f2b62d06fd30234bb18afbf
Maccio92 <valerio362@gmail.com>
2022-04-11 09:22:12 +0200

    fix admin login message

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -4,10 +4,12 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+            <div class="card mt-5 border-0">
+                <div class="card-header border-0"> 
+                    <h1 class="text-info">Benvenuto nella tua Area Personale!</h1>
+                </div>
 
-                <div class="card-body">
+                <!-- <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
@@ -15,7 +17,7 @@
                     @endif
 
                     {{ __('You are logged in!') }}
-                </div>
+                </div> -->
             </div>
         </div>
     </div>
